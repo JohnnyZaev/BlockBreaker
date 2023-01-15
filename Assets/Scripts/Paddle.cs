a75254b49f8bbac399312949df4295ddd7fe6347
gvarys <johnnyzaev@gmail.com>
2023-01-15 11:22:22 +0600

    Clamped the movement of paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
 	[SerializeField] private float screenWidthInUnits = 16f;
+	[SerializeField] private float minX = 1f;
+	[SerializeField] private float maxX = 15f;
 
 	private Vector2 _paddlePos;
 
 	private void Update()
 	{
 		var mousePosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-		_paddlePos.x = mousePosition;
+		_paddlePos.x = Mathf.Clamp(mousePosition, minX, maxX);
 		var paddleTransform = transform;
 		_paddlePos.y = paddleTransform.position.y;
 		paddleTransform.position = _paddlePos;
