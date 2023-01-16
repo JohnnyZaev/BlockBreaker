4a6f0ee07d59ecf554b63410bd90a32ce752b177
gvarys <johnnyzaev@gmail.com>
2023-01-16 12:57:06 +0600

    Add score and move it to UI and all levels

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,16 +5,24 @@ public class Block : MonoBehaviour
 	[SerializeField] private AudioClip breakSound;
 
 	private Level _level;
+	private GameStatus _gameStatus;
 
 	private void Start()
 	{
 		_level = FindObjectOfType<Level>();
 		_level.CountBreakableBlocks();
+		_gameStatus = FindObjectOfType<GameStatus>();
 	}
 
 	private void OnCollisionEnter2D(Collision2D col)
 	{
+		BlockDestroyed();
+	}
+
+	private void BlockDestroyed()
+	{
 		_level.BlockDestroyed();
+		_gameStatus.AddToScore();
 		AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
 		Destroy(gameObject);
 	}
---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+	[Range(0.1f, 10)][SerializeField] private float gameSpeed = 1f;
+	[SerializeField] private TMP_Text scoreText;
+	
+	// state variables
+	[SerializeField] private int pointsPerBlockDestroyed = 15;
+	[SerializeField] private int currentScore;
+
+	private void Start()
+	{
+		scoreText.text = currentScore.ToString();
+	}
+
+	private void Update()
+	{
+		Time.timeScale = gameSpeed;
+	}
+
+	public void AddToScore()
+	{
+		currentScore += pointsPerBlockDestroyed;
+		scoreText.text = currentScore.ToString();
+	}
+}
