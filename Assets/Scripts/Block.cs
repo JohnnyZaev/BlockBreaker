e296cb0cc63b731059619a201f0f43c245d13f10
gvarys <johnnyzaev@gmail.com>
2023-01-18 13:21:11 +0600

    hit count is now automatic and bug with 0 hit block is fixed

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,20 +5,21 @@ public class Block : MonoBehaviour
 	//config params
 	[SerializeField] private AudioClip breakSound;
 	[SerializeField] private GameObject blockSparklesVFX;
-	[SerializeField] private int maxHits;
 	[SerializeField] private Sprite[] hitSprites;
 
 	// cached reference
 	private Level _level;
 	private GameStatus _gameStatus;
-	
+
 	//state variable
 	[SerializeField] private int timesHit; // TODO only serialize for debug
+	private int maxHits;
 
 	private void Start()
 	{
 		if (gameObject.CompareTag("Unbreakable"))
 			return;
+		maxHits = hitSprites.Length + 1;
 		_level = FindObjectOfType<Level>();
 		_level.CountBreakableBlocks();
 		_gameStatus = FindObjectOfType<GameStatus>();
@@ -42,7 +43,14 @@ public class Block : MonoBehaviour
 	private void ShowNextHitSprite()
 	{
 		var spriteIndex = timesHit - 1;
-		GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		if (hitSprites[spriteIndex] != null)
+		{
+			GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
+		else
+		{
+			Debug.LogError($"Block sprite is missing from array: {gameObject.name}");
+		}
 	}
 
 	private void BlockDestroyed()
