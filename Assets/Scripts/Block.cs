ffc9f1a22da0473fc7b6cb72c407bb0d2c1d5753
gvarys <johnnyzaev@gmail.com>
2023-01-18 11:50:56 +0600

    VFX now playing and Destroying after few seconds

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
 	[SerializeField] private AudioClip breakSound;
+	[SerializeField] private GameObject blockSparklesVFX;
 
 	private Level _level;
 	private GameStatus _gameStatus;
@@ -21,9 +22,17 @@ public class Block : MonoBehaviour
 
 	private void BlockDestroyed()
 	{
+		TriggerSparklesVFX();
 		_level.BlockDestroyed();
 		_gameStatus.AddToScore();
 		AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
 		Destroy(gameObject);
+	}
+
+	private void TriggerSparklesVFX()
+	{
+		var transform1 = transform;
+		var sparkles = Instantiate(blockSparklesVFX, transform1.position, transform1.rotation);
+		Destroy(sparkles, 1f);
 	}
 }
